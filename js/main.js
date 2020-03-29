69989dc0f8903295002eb8c2b7d85ef0eb50c235
Dmitrii <matvey-DP@yandex.ru>
2020-03-29 16:37:36 +0300

    feat: добавили валидацию для form

--- js/main.js
+++ js/main.js
@@ -44,4 +44,36 @@ $(document).ready(function(){
   bullets.css('left', prev.width() + 10)
 
   new WOW().init();
+
+  //валидация формы
+  $('.modal__form').validate({
+    errorClass: "invalid",
+    rules: {
+      // строчное правило
+      userPhone: "required",
+      // правило-объект
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userEmail: {
+        required: true,
+        email: true
+      }
+    }, //сообщения
+    messages: {
+      userName: {
+        required: "*Имя обязательно",
+        minlength: "*Имя не короче 2-х букв"
+      },
+      userPhone: "*Телефон обязателен",
+      userEmail: {
+        required: "*Обязательно укажите email",
+        email: "*Введите в формате: name@domain.com"
+      }
+    }
+  });
+
+  //маска для телефона
+  $('[type=tel]').mask(' +7 (000) 000 - 00 - 00', {placeholder: " +7 (___) ___ - __ - __"});
 });
