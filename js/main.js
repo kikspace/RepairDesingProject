29812c8acc050f35f9cb51080b4215d8d6297672
Dmitrii <matvey-DP@yandex.ru>
2020-03-31 18:02:37 +0300

    feat: настроили форму отправка, закрытие

--- js/main.js
+++ js/main.js
@@ -73,6 +73,18 @@ $(document).ready(function(){
         required: "*Обязательно укажите email",
         email: "*Введите в формате: name@domain.com"
       }
+    },
+    submitHandler: function(form) {
+      $.ajax({
+        type: "POST",
+        url: "send.php",
+        data: $(form).serialize(),
+        success: function (response) {
+          alert('Форма отправлена, мы свяжемся с Вами через 10 минут');
+          $(form)[0].reset();
+          modal.removeClass('modal--visible');
+        }
+      });
     }
   });
 
